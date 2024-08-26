80ce1a8a612e205226f3bc8ca42d0351f960fdd8
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2024-08-26 18:28:12 +0200

    ✨ Initial models

--- clinica/__init__.py
+++ clinica/__init__.py
@@ -9,10 +9,32 @@ This is the complete API reference:
 .. autosummary::
    :toctree: .
 
-   example_function
-   ExampleClass
+   Biosample
+   Medication
+   Patient
+   Project
+   Treatment
 """
 
 __version__ = "0.0.1"  # denote a pre-release for 0.1.0 with 0.1rc1
 
-from ._core import ExampleClass, example_function
+from lamindb_setup import _check_instance_setup
+
+
+# trigger instance loading if users
+# want to access attributes
+def __getattr__(name):
+    if name not in {"models"}:
+        _check_instance_setup(from_lamindb=True)
+    return globals()[name]
+
+
+if _check_instance_setup():
+    del __getattr__  # delete so that imports work out
+    from .models import (
+        Biosample,
+        Medication,
+        Patient,
+        Project,
+        Treatment,
+    )
