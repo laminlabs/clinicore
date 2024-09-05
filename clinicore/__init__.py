be96ad0c92b576dd769f2a58167f6f509fdb12e7
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2024-09-05 21:25:51 +0200

    🔖 Release 0.3.0

--- clinicore/__init__.py
+++ clinicore/__init__.py
@@ -16,7 +16,7 @@ This is the complete API reference:
    Treatment
 """
 
-__version__ = "0.2.0"  # denote a pre-release for 0.1.0 with 0.1rc1
+__version__ = "0.3.0"  # denote a pre-release for 0.1.0 with 0.1rc1
 
 from lamindb_setup import _check_instance_setup
 
